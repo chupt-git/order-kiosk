c002a1743dbc36d17aef9439aeb9d4056a786fc0
Chelsea Dover <chelseamegandover@gmail.com>
2019-03-25 16:56:39 -0700

    order info screen main style

--- chupt-kiosk/screens/OrderInfo.js
+++ chupt-kiosk/screens/OrderInfo.js
@@ -6,8 +6,10 @@ import HeaderText from '../components/HeaderText'
 import ColoredText from '../components/ColoredText'
 import MainButton from '../components/MainButton'
 import ButtonText from '../components/ButtonText'
-import TopNavigation from "./TopNavigation";
+import TopNavigation from "./TopNavigation"
 import Body from '../components/Body'
+import Txt from "../components/Txt"
+import PickupButton from '../components/PickupButton'
 
 class OrderInfo extends React.Component {
 
@@ -16,29 +18,73 @@ class OrderInfo extends React.Component {
             <MainWrap>
                 <TopNavigation/>
                 <Body>
-                <View style={{width: '97%', height: '90%', display: 'flex', justifyContent: 'space-around'}}>
-                    <View>
+                    <View style={{width: '97%', height: '90%', display: 'flex', justifyContent: 'space-around'}}>
                         <View>
-                            <HeaderText left margin big>Pickup</HeaderText>
+                        <HeaderText style={{marginBottom: 50}} left big>Pickup</HeaderText>
 
+                            <View style={{display:'flex', flexDirection: 'row', alignItems: 'center'}}>
+                                <PickupButton style={{height: 150}} status='full'>
+                                     <ColoredText bigger>12</ColoredText>
+                                </PickupButton>
+
+                                <View style={{
+                                    marginLeft: 10,
+                                    height: 150,
+                                    display: 'flex',
+                                    justifyContent: 'space-around'
+                                }}>
+                                    <Txt bold>Jane Doe</Txt>
+                                    <Txt bold>541-602-6215</Txt>
+                                    <Txt bold>Order ID: 12345</Txt>
+                                </View>
+                            </View>
+
+                            <View style={{marginLeft: 50, marginTop: 50}}>
+                                <View>
+                                    <Txt bold blue style={{marginBottom: 10}}>Meals:</Txt>
+
+                                    <View style={{marginLeft: 10}}>
+                                        <Txt style={{maxWidth: '50%'}} light>
+                                            Meal Name
+                                        </Txt>
+                                        <View style={{marginLeft: 10}}>
+                                            <Text style={{marginBottom: 2, color:'#919191'}}>
+                                                - No Sauce
+                                            </Text>
+                                        </View>
+                                    </View>
+                                </View>
+
+                                <View>
+                                    <Txt bold blue style={{marginBottom: 10}}>Entrees:</Txt>
+
+                                    <View style={{marginLeft: 10}}>
+                                        <Txt style={{maxWidth: '50%'}} light>
+                                            Entree Name
+                                        </Txt>
+                                        <View style={{marginLeft: 10}}>
+                                            <Text style={{marginBottom: 2, color:'#919191'}}>
+                                                - Mild For Heat
+                                            </Text>
+                                            <Text style={{marginBottom: 2, color:'#919191'}}>
+                                                - No Carrot
+                                            </Text>
+                                        </View>
+                                    </View>
+                                </View>
+                            </View>
                         </View>
 
-                    </View>
-                    <View style={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        width: '100%'
-                    }}>
+
                         <MainButton
                             noBorder
                             green
-                            medWidth
+                            fullWidth
                             centerText
                             onPress={() => this.props.navigation.navigate('Checkout')}>
                             <ColoredText>Open Locker</ColoredText>
                         </MainButton>
                     </View>
-                </View>
                 </Body>
             </MainWrap>
         )
@@ -46,3 +92,20 @@ class OrderInfo extends React.Component {
 }
 
 export default OrderInfo
+
+// <View style={{display:'flex', flexDirection: 'row'}}>
+// <PickupButton status='full'>
+//     <ColoredText bigger>12</ColoredText>
+// </PickupButton>
+// <View>
+// <Text>Jane Doe</Text>
+// <Text>541-602-6215</Text>
+// <Text>Order ID: 12345</Text>
+// </View>
+// </View>
+
+// <View style={{
+//     display: 'flex',
+//         alignItems: 'center',
+//         width: '100%'
+// }}>
